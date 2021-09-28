fdc895ac3db91752ed2d47810681c1c361016a73
rdbarker <ryan.dean.barker@gmail.com>
2021-09-28 18:10:06 -0400

    pressing number updates display

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const documentScreen = document.querySelector(".screen");
 const documentNumberButtons = document.querySelectorAll(".button-number");
+const documentOperatorButtons = document.querySelectorAll(".button-operator");
+const documentFunctionButtons = document.querySelectorAll(".button-function");
+
 
 function updateScreen(text){
     documentScreen.textContent+=text;
@@ -8,3 +11,9 @@ function deleteLastNumber(){
     const currentText = documentScreen.textContent;
     documentScreen.textContent = currentText.slice(0,-1);
 }
+
+function pressedNumber(e){
+    updateScreen(this.dataset["value"]);
+}
+
+documentNumberButtons.forEach(item => item.addEventListener("click",pressedNumber));
